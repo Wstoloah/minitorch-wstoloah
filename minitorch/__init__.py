aec2ef19ca2dbba62a20a400da2357cc29cf1d5e
Ouissal <ouissal.boutouatou@student-cs.fr>
2025-07-23 21:18:02 +0200

    tests for CNN and nn implementations

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -19,3 +19,6 @@ from .tensor_functions import *  # noqa: F401,F403
 from .cuda_ops import *  # noqa: F401,F403
 from .fast_ops import *  # noqa: F401,F403
 from . import fast_ops, cuda_ops  # noqa: F401,F403
+
+from .nn import *  # noqa: F401,F403
+from .fast_conv import *  # noqa: F401,F403
